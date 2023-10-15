4fd3b5ea7976611a1be4807e72834165451b0297
MSK4477 <ks7997067@gmail.com>
2023-10-16 00:25:43 +0530

    cors

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
     console.log(data)
     return (
         <div className="h-screen flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-bold  mb-4">Welcome {data.name} to My inventory app!</h1>
+            <h1 className="text-4xl font-bold  mb-4">Welcome to My inventory app <span className="text-blue-600">{data.name}</span>!</h1>
             <p className="text-lg mb-8">Here you can manage your inventory and keep track of your products.</p>
             <button onClick={navigateToHome} className="bg-blue-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
                 Get started
