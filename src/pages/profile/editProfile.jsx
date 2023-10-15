562e371d86d1bddfcdfc5c1d99c5c695abffb1dc
MSK4477 <ks7997067@gmail.com>
2023-10-16 00:37:26 +0530

    added cors

--- src/pages/profile/editProfile.jsx
+++ src/pages/profile/editProfile.jsx
@@ -45,50 +45,55 @@ const EditProfile = () => {
     <>
       <div className=" w-[calc(100%-16%)] absolute left-48 top-0 p-9  h-screen grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2  ">
         <form
-          className="col-span-1  justify-center items-center h-auto bg-white m-2 p-8 rounded-lg flex-col flex shadow-2xl"
+          className="col-span-1 relative  justify-center items-center h-auto bg-white m-2 p-8 rounded-lg flex-col flex shadow-2xl"
           onSubmit={handleSubmit}
         >
-          <h1 className="text-2xl font-bold">Update User</h1>
+          <h1 className="text-2xl absolute top-3 font-bold">Update User</h1>
 
-          <label className=" p-2 text-xl text-gray-700">User Name</label>
 
           <input
             type="text"
             name="name"
+            required={true}
+            placeholder="Name"
             value={user.name}
             onChange={handleChange}
-            className=" p-2 border w-3/4 border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            className=" p-4 border rounded-lg w-3/4 border-gray-300  focus:outline-none focus:border-blue-500"
           />
 
-          <label className="p-2 text-xl text-gray-700">Phone</label>
-
+<br /><br />
           <input
             type="text"
+            required={true}
+            placeholder="Phone"
             name="phone"
             value={user.phone}
             onChange={handleChange}
-            className=" p-2 border w-3/4 border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            className=" p-4 border rounded-lg w-3/4 border-gray-300  focus:outline-none focus:border-blue-500"
           />
-          <label className="p-2 text-xl text-gray-700">Image</label>
-
+<br /><br />
           <input
             type="text"
+
+            required={true}
+            placeholder="image"
             name="category"
             value={user.image}
             onChange={handleChange}
-            className=" p-2 border w-3/4 border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            className=" p-4 border w-3/4 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
           />
+          <br /><br />
 
           <br />
           <button
-            className="bg-blue-500 w-1/2 rounded-lg text-white p-2 hover:bg-sky-700"
+            className="bg-blue-500 w-1/2  rounded-lg text-white p-4 hover:bg-sky-700"
             type="submit"
           >
             Update
           </button>
         </form>
 
-        <div className="col-span-1  justify-center items-center h-fit bg-white m-2 p-8 rounded-lg flex-col flex shadow-2xl">
+        <div className="  justify-center items-center h-fit bg-white m-2 p-8 rounded-lg flex-col flex shadow-2xl">
         <img src={inv6} alt="" />
       </div>
       </div>
