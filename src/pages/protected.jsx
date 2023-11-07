188b84369ff16c252d39afef472e71ea69d01c7d
MSK4477 <ks7997067@gmail.com>
2023-11-07 23:25:20 +0530

    completed

--- src/pages/protected.jsx
+++ src/pages/protected.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const ProtectedPage = ({ element }) => {
 
-  if (!localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'))) {
+  if (localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'))) {
         return element;
   }
 
