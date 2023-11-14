e3aab81d46ff59c6347f423259aa95262477da41
MSK4477 <ks7997067@gmail.com>
2023-11-14 13:08:25 +0530

    completed

--- src/pages/userAuth/verify.jsx
+++ src/pages/userAuth/verify.jsx
@@ -2,14 +2,17 @@ import { toast } from 'react-toastify';
 import { useEffect, useState } from 'react'; 
 import { useSearchParams, Link } from 'react-router-dom';
 import { verify } from '../../service/userService/authService';
+import Loader from "../loader.jsx";
+
 const Verify = () => {
 
     const[ queryParams ] = useSearchParams();
     const [load, setLoad] = useState(true)
     const token = queryParams.get('token');
+    const [loader, setLoader] = useState(false)
 
 const verifyUser = async () => {
-
+setLoader(true)
     try{
         
         const res = await verify(token)
@@ -22,28 +25,23 @@ const verifyUser = async () => {
 console.log(res)
     }catch(err){
         console.log(err)
+    } finally { 
+        setLoader(false)
     }
 
 }
 
 useEffect(() => { 
     verifyUser()
-})
+}, [])
 
 return(
     <>
-    {load ?(<div className='   flex justify-center h-screen items-center'> 
+    {!loader &&(<div className='   flex justify-center h-screen items-center'> 
       <h1 className='w-auto py-12 px-10 font-serif rounded-lg shadow-lg font-bold text-3xl'>
         Email Verified go to <Link className=" font-serif text-blue-500 hover:text-sky-700" to={"/login"}>Login</Link> Page  </h1>  
-        </div>) : (
-<div className=' font-serif flex justify-center  h-screen items-center '>
-            <div className=' bg-slate-200  w-auto py-12 px-10 rounded-lg shadow-lg'>
-            <h1 className='font-bold text-5xl font-sans text-black'>
-       Wait...
-        </h1>
-        </div>
-        </div>
-        )}
+        </div>)} { loader &&
+            <div className="flex justify-center items-center w-full h-screen bg-white"><Loader /> </div>        }
         </>
 )
 
